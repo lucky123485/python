33c85b18e8bf99021c48e14ee22ac94869c01772
Bhavesh Sharma <151143087+lucky123485@users.noreply.github.com>
2024-05-03 18:06:37 +0530

    Create sens.py

---
+++ sens.py
@@ -0,0 +1,16 @@
+# secrets.py
+
+# Example of a secret API key
+api_key = "abc123xyz456"
+
+# Example of a password
+password = "MyPa$$w0rd"
+
+# Example of an AWS access key
+aws_access_key = "AKIA1234567890"
+
+# Example of an encryption key
+encryption_key = "encrypt123key456789"
+
+# Example of a database connection string
+db_connection_string = "mysql://username:password@hostname:port/database_name"
